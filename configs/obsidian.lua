cf66ded19ac43d4a8428a40af2f9db0069a34b61
Julian Fahrer <julian@fahrer.net>
2024-03-05 21:20:44 -0800

    Add obsidian config

---
+++ configs/obsidian.lua
@@ -0,0 +1,16 @@
+M = {}
+
+M.opts = {
+  workspaces = {
+    {
+      name = "cooking",
+      path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Cooking",
+    },
+    {
+      name = "meraki",
+      path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Meraki",
+    },
+  },
+}
+
+return M
